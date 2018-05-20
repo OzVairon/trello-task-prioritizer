ce03b68045316f31b99e81e595044ddaa121dfe2
ozvairon <vairon3004@gmail.com>
2018-05-21 02:12:04 +0300

    change routes for internal views

--- public/js/client.js
+++ public/js/client.js
@@ -418,34 +418,17 @@ TrelloPowerUp.initialize({
       
   */
   'authorization-status': function(t, options){
-    // Return a promise that resolves to an object with a boolean property 'authorized' of true or false
-    // The boolean value determines whether your Power-Up considers the user to be authorized or not.
-    
-    // When the value is false, Trello will show the user an "Authorize Account" options when
-    // they click on the Power-Up's gear icon in the settings. The 'show-authorization' capability
-    // below determines what should happen when the user clicks "Authorize Account"
-    
-    // For instance, if your Power-Up requires a token to be set for the member you could do the following:
-    return t.get('member', 'private', 'token')
     .then(function(token){
       if(token){
         return { authorized: true };
       }
       return { authorized: false };
     });
-    // You can also return the object synchronously if you know the answer synchronously.
   },
   'show-authorization': function(t, options){
-    // Returns what to do when a user clicks the 'Authorize Account' link from the Power-Up gear icon
-    // which shows when 'authorization-status' returns { authorized: false }.
     
-    // If we want to ask the user to authorize our Power-Up to make full use of the Trello API
-    // you'll need to add your API from trello.com/app-key below:
     let trelloAPIKey = '';
-    // This key will be used to generate a token that you can pass along with the API key to Trello's
-    // RESTful API. Using the key/token pair, you can make requests on behalf of the authorized user.
     
-    // In this case we'll open a popup to kick off the authorization flow.
     if (trelloAPIKey) {
       return t.popup({
         title: 'My Auth Popup',
@@ -459,4 +442,4 @@ TrelloPowerUp.initialize({
   }
 });
 
-console.log('Loaded by: ' + document.referrer);
+console.log('Loaded by OZV: ' + document.referrer);
