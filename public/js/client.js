bc035f04cbf7e226334a16d0905f6e53044ada2e
ozvairon <vairon3004@gmail.com>
2018-05-21 01:58:28 +0300

    add sorter

--- public/js/client.js
+++ public/js/client.js
@@ -289,6 +289,31 @@ TrelloPowerUp.initialize({
       return [];
     }
   },
+  'list-sorters': function (t) {
+    return t.list('name', 'id')
+    .then(function (list) {
+      return [{
+        text: "Card Name",
+        callback: function (t, opts) {
+          // Trello will call this if the user clicks on this sort
+          // opts.cards contains all card objects in the list
+          var sortedCards = opts.cards.sort(
+            function(a,b) {
+              if (a.name > b.name) {
+                return 1;
+              } else if (b.name > a.name) {
+                return -1;
+              }
+              return 0;
+            });
+          
+          return {
+            sortedIds: sortedCards.map(function (c) { return c.id; })
+          };
+        }
+      }];
+    });
+  },
   'attachment-thumbnail': function(t, options){
     // options.url has the url of the attachment for us
     // return an object (or a Promise that resolves to it) with some or all of these properties:
