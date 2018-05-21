008193e735dc8cfb3ad5fe1ec0f809ee43d3df23
ozvairon <vairon3004@gmail.com>
2018-05-21 04:49:34 +0300

    add effort field2

---
+++ public/js/params.js
@@ -0,0 +1,30 @@
+/* global TrelloPowerUp */
+
+var Promise = TrelloPowerUp.Promise;
+var t = TrelloPowerUp.iframe();
+
+var hoursField = document.getElementById('hours');
+
+
+t.render(function(){
+  return Promise.all([
+    t.get('card', 'shared', 'fruit')
+  ])
+  .spread(function(hours_data){
+    console.log(hours_data)
+    if (hours_data) hoursField.value = hours_data
+    else hoursField.value = 0;
+    
+  })
+  .then(function(){
+    t.sizeTo('#content')
+    .done();
+  })
+});
+
+document.getElementById('save').addEventListener('click', function(){
+  return t.set('card', 'shared', 'effort_hours', hoursField.value)
+  .then(function(){
+    t.closePopup();
+  })
+})
