6a2c72f69aaa1209f5961b5d7e2310ba75da9cdd
ozvairon <vairon3004@gmail.com>
2018-05-28 04:07:13 +0300

    change type of script

--- index.js
+++ index.js
@@ -4,12 +4,27 @@ const PORT = process.env.PORT || 5000
 const app = express();
 
 app.use(express.static(path.join(__dirname, 'public')))
+app.use( function(req, res, next) {
+
+  if (req.originalUrl && req.originalUrl.split("/").pop() === 'favicon.ico') {
+    return res.sendStatus(204);
+  }
+
+  return next();
+
+});
 app.set('views', path.join(__dirname, 'views'))
 app.set('view engine', 'ejs')
 app.get('/', (req, res) => res.render('pages/index'))
 app.get('/numeric', (req, res) => {
   	console.log('try to render numeric form')
-  	res.render('pages/numeric_form')
+
+  	let data = {
+  		value: req.queue.value,
+  		name: req.queue.name
+  	};
+
+  	res.render('pages/numeric_form', {data: data})
   })
 app.listen(PORT, () => console.log(`Listening on ${ PORT }`))
 
