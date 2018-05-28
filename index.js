eb64612b7dfb52b0e0de29e96026558bf1f1e3d3
ozvairon <vairon3004@gmail.com>
2018-05-28 03:38:54 +0300

    try to use generic forms

--- index.js
+++ index.js
@@ -1,16 +1,15 @@
 const express = require('express')
 const path = require('path')
 const PORT = process.env.PORT || 5000
+const app = express();
 
-express()
-  .use(express.static(path.join(__dirname, 'public')))
-  .set('views', path.join(__dirname, 'views'))
-  .set('view engine', 'ejs')
-  .get('/', (req, res) => res.render('pages/index'))
-  .listen(PORT, () => console.log(`Listening on ${ PORT }`))
-
-express()
-  .get('/numeric', (req, res) => {
+app.use(express.static(path.join(__dirname, 'public')))
+app.set('views', path.join(__dirname, 'views'))
+app.set('view engine', 'ejs')
+app.get('/', (req, res) => res.render('pages/index'))
+app.get('/numeric', (req, res) => {
   	console.log('try to render numeric form')
   	res.render('pages/numeric_form')
   })
+app.listen(PORT, () => console.log(`Listening on ${ PORT }`))
+
