7af670fbe51c91fa7848ff5543e21d29792b3fe6
ozvairon <vairon3004@gmail.com>
2018-06-05 23:32:11 +0300

    try to use sections

--- public/js/section.js
+++ public/js/section.js
@@ -1,30 +1,6 @@
 /* global TrelloPowerUp */
-let Promise = TrelloPowerUp.Promise;
 let t = TrelloPowerUp.iframe();
-
-// you can access arguments passed to your iframe like so
-//var arg = t.arg('arg');
-
-// t.render(function(){
-
-//   console.log('t.render')
-
-//   t.card('attachments')
-//   .get('attachments')
-//   .filter(function(attachment){
-//     return attachment.url.indexOf('https://trello.com/c/') == 0;
-//   })
-//   .then(function(yellowstoneAttachments){
-//     var urls = yellowstoneAttachments.map(function(a){ return a.url; });
-//     document.getElementById('urls').textContent = urls.join(', ');
-//   })
-//   .then(function(){
-//     return t.sizeTo('#content');
-//   });
-//   //render_func()
-// });
-
-
+console.log(t.getContext())
 t.render(function render() {
   t.card('attachments')
     .get('attachments')
@@ -42,40 +18,4 @@ function render_func() {
 
   console.log('promice object')
   console.log(t.card('attachments'))
-
-
-  // .then(a => console.log("a: " + a))
-  // .catch((err)=> {
-  //     console.log('error')
-  //     console.log(err)
-  //   })  
-  
-  
-
-
-  // console.log('attachment render')
-  // console.log(t)
-  
-  // console.log(t.card('all'));
-
-  // Promise.resolve(t.card('all'))
-  //   .then(function () {
-  //     console.log('hui');
-  //   })
-  //   .catch((err) => {
-  //     console.log(err.statusText)
-  //   })
-  // .get('attachments')
-  // .filter(function(attachment){
-  //   console.log(attachment)
-  //   return attachment.url.indexOf('https://trello.com/c/') == 0;
-  // })
-  // .then(function(cards){
-  //   console.log(cards)
-  //   var urls = cards.map(function(a){ return a.url; });
-  //   document.getElementById('urls').textContent = urls.join(', ');
-  // })
-  // .then(function(){
-  //   return t.sizeTo('#content');
-  // });
 }
