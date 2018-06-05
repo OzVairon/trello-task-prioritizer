3f69d02e84a3709f008128e5d8c1fb8ebbe0cffa
ozvairon <vairon3004@gmail.com>
2018-06-06 00:21:31 +0300

    try to use sections

--- public/js/section.js
+++ public/js/section.js
@@ -12,6 +12,16 @@ t.render(function render() {
 
 document.addEventListener("DOMContentLoaded", function() {
   t = TrelloPowerUp.iframe();
+
+  t.render(function render() {
+    t.card('attachments')
+      .get('attachments')
+      .then(() => {
+        console.log("HUI")
+      })
+  });
+
+  
   console.log('ready')
   console.log(t)
   console.log(t.getContext())
