42e7bcf0024f6000da57f393fba9f0af49c7820b
ozvairon <vairon3004@gmail.com>
2018-06-06 04:04:03 +0300

    add related cards functionality

--- public/js/section.js
+++ public/js/section.js
@@ -18,11 +18,13 @@ document.addEventListener("DOMContentLoaded", function() {
     t.card('attachments')
     .get('attachments')
     .filter(function(attachment){
-      return attachment.url.indexOf('https://trello.com/c/') == 0;
+      return (attachment.url.indexOf('https://trello.com/c/') == 0 && attachment.url.substring(base.length).length === 24);
     })
-    .then(function(yellowstoneAttachments){
-      var urls = yellowstoneAttachments.map(function(a){ return a.url; });
-      document.getElementById('urls').textContent = urls.join(', ');
+    .then(function(related){
+
+      create_card_view(related.name, related.url)
+      //var urls = related.map(function(a){ return a.url; });
+      //document.getElementById('urls').textContent = urls.join(', ');
     })
     .then(function(){
       return t.sizeTo('#content');
@@ -35,3 +37,23 @@ document.addEventListener("DOMContentLoaded", function() {
   console.log(t.getContext())
   console.log('ready end')
 });
+
+function create_card_view(name, url) {
+
+  let card_html = (
+    `<li class = ''>
+      <a href = '${url}'>
+        <div class ='card-back'>
+          <div><span class='card-title'>${name}</span></div>
+          <div><div class='delete-att-button'>x</div></div>
+        </div>
+      </a>
+    </li>`
+  )
+
+  var newNode = document.createElement('li');
+  newNode.className = 'card-wrapper';
+  newNode.innerHTML = card_html
+  document.getElementById('related-card-list').appendChild(newNode);  
+  
+} 
