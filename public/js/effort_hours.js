471238eca8e60fe63fa997406d13680cc2a16da9
ozvairon <vairon3004@gmail.com>
2018-06-06 09:12:19 +0300

    try to found error with custom promises

--- public/js/effort_hours.js
+++ public/js/effort_hours.js
@@ -7,31 +7,39 @@ let field = document.getElementById('numeric_field');
 
 
 t.render(function(){
-  console.log('ready')
-  console.log(t)
-  console.log(t.getContext())
-  console.log('ready end')
-  return Promise.all([
-    t.get('card', 'shared', key)
-  ])
-  .spread(function(data){
+  
+  // return Promise.all([
+  //   t.get('card', 'shared', 'effort_hours')
+  // ])
+  // .spread(function(data){
+  //   if (data) field.value = data
+  //   else field.value = 0;
+  // })
+  // .then(function(){
+  //   t.sizeTo('#content')
+  //   .done();
+  // })
+
+  
+  t.get('card', 'shared', 'effort_hours')
+  .then((hours)=>{
     if (data) field.value = data
     else field.value = 0;
-    
   })
-  .then(function(){
+  .then(() => {
     t.sizeTo('#content')
     .done();
   })
 });
 
 document.getElementById('save').addEventListener('click', function(){
-  return t.set('card', 'shared', key, field.value)
+  console.log(t.getContext())
+  return t.set('card', 'shared', 'effort_hours', field.value)
   .then(function(){
       t.closePopup();
     }, function(err){
       console.log('rejected')
       alert(err.message)
     }
-  )
+  ).done();
 })
