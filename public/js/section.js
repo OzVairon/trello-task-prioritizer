30dde3517905ded06096b9149b8fd0479f6cb768
ozvairon <vairon3004@gmail.com>
2018-06-06 14:33:11 +0300

    related cards section

--- public/js/section.js
+++ public/js/section.js
@@ -14,17 +14,11 @@ t.render(function render() {
     t.cards('all').then( (cards) => {
       document.getElementById('related-card-list').innerHTML = ""
       related.forEach( rc => {
-        console.log('p1')
-        console.log(rc)
         let theCard = cards.filter((e) => {return e.id == rc.name})[0]
         if (theCard) {
-          console.log('p2.1')
-          console.log(rc)
-
           create_card_view(theCard.name, theCard.id, rc.id)
         }
         else {
-          console.log('p2.2')
           create_card_view(rc.name, rc.name, rc.id)
         }
       })
@@ -43,7 +37,6 @@ t.render(function render() {
 
 function create_card_view(name, card_id, att_id) {
 
-  console.log('1')
   let card_html = (
     `<div class ='card-back'>
       <div><span class='card-title'>${name}</span></div>
@@ -55,12 +48,10 @@ function create_card_view(name, card_id, att_id) {
   newNode.className = 'card-wrapper';
   newNode.innerHTML = card_html
 
-  console.log('2')
-  document.getElementById('related-card-list').appendChild(newNode);  
-  newNode.addEventListener('click', function(){
-    t.showCard(card_id)
-  })
-  console.log('3')
+  // document.getElementById('related-card-list').appendChild(newNode);  
+  // newNode.addEventListener('click', function(){
+  //   t.showCard(card_id)
+  // })
   newNode.getElementsByClassName('delete-att-button')[0].addEventListener('click', function(){
     t.card('id').then(s_card_id => {
       console.log(s_card_id)
